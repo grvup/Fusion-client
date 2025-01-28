14ddcf59aa5742917a08fb4b531b5c78dc78c415
haarsh157 <harshrange123@gmail.com>
2025-01-28 17:53:21 +0530

    Made course instructors page and form

---
+++ src/Modules/Program_curriculum/Acad_admin/Admin_view_all_course_instructors.jsx
@@ -0,0 +1,295 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import {
+  MantineProvider,
+  Table,
+  Flex,
+  Container,
+  Button,
+  TextInput,
+  Grid,
+  Paper,
+} from "@mantine/core";
+// import { fetchAllCourseInstructors } from "../api/api";
+
+function Admin_view_all_course_instructors() {
+  const [searchName, setSearchName] = useState("");
+  const [searchVersion, setSearchVersion] = useState("");
+  const [instructors, setInstructors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("authToken");
+        if (!token) {
+          throw new Error("Authorization token not found");
+        }
+
+        // const data = await fetchAllCourseInstructors(token);
+        // setInstructors(data.instructors);
+        // console.log(data);
+      } catch (error) {
+        console.error("Error fetching instructors: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Filtered data based on search inputs
+  const filteredData = instructors.filter(
+    (item) =>
+      item.name.toLowerCase().includes(searchName.toLowerCase()) &&
+      item.version.toString().includes(searchVersion),
+  );
+
+  // Define alternating row colors
+  const rows = filteredData.map((element, index) => (
+    <tr
+      key={element.name + element.version}
+      style={{ backgroundColor: index % 2 === 0 ? "#FFFFFF" : "#E6F7FF" }}
+    >
+      <td
+        style={{
+          padding: "15px 20px",
+          textAlign: "center",
+          color: "#3498db",
+          textDecoration: "underline",
+          cursor: "pointer",
+          borderRight: "1px solid #d3d3d3",
+        }}
+      >
+        {/* Curriculum name as a link */}
+        <Link
+          to={`/programme_curriculum/view_curriculum?curriculum=${element.id}`}
+          style={{ color: "#3498db", textDecoration: "underline" }}
+        >
+          {element.name}
+        </Link>
+      </td>
+      <td
+        style={{
+          padding: "15px 20px",
+          textAlign: "center",
+          borderRight: "1px solid #d3d3d3",
+        }}
+      >
+        {element.version}
+      </td>
+      <td
+        style={{
+          padding: "15px 20px",
+          borderRight: "1px solid #d3d3d3",
+          textAlign: "center",
+        }}
+      >
+        {element.batch && element.batch.length > 0 ? (
+          element.batch.map((b, i) => <div key={i}>{b}</div>)
+        ) : (
+          <div>No batches available</div>
+        )}
+      </td>
+      <td
+        style={{
+          padding: "15px 20px",
+          textAlign: "center",
+          borderRight: "1px solid #d3d3d3",
+        }}
+      >
+        {element.semesters}
+      </td>
+      <td
+        style={{
+          padding: "15px 20px",
+          textAlign: "center",
+        }}
+      >
+        {/* Edit button as a link */}
+        <Link to="/programme_curriculum/admin_edit_curriculum_form">
+          <Button variant="filled" color="green" radius="md">
+            EDIT
+          </Button>
+        </Link>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <MantineProvider
+      theme={{ colorScheme: "light" }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Container
+        style={{ padding: "20px", minHeight: "100vh", maxWidth: "100%" }}
+      >
+        <Flex justify="flex-start" align="center" mb={20}>
+          <Button variant="filled" style={{ marginRight: "10px" }}>
+            Instructors
+          </Button>
+        </Flex>
+
+        <Grid>
+          <Grid.Col span={9}>
+            {/* Table Section */}
+            <div
+              style={{
+                height: "500px",
+                overflowY: "auto",
+                border: "1px solid #d3d3d3",
+                borderRadius: "10px",
+              }}
+            >
+              <Table
+                style={{
+                  backgroundColor: "white",
+                  padding: "20px",
+                  flexGrow: 1,
+                }}
+              >
+                <thead>
+                  <tr>
+                    <th
+                      style={{
+                        padding: "12px 20px",
+                        backgroundColor: "#C5E2F6",
+                        color: "#3498db",
+                        fontSize: "16px",
+                        textAlign: "center",
+                        borderRight: "1px solid #d3d3d3",
+                      }}
+                    >
+                      Course Code
+                    </th>
+                    <th
+                      style={{
+                        padding: "12px 20px",
+                        backgroundColor: "#C5E2F6",
+                        color: "#3498db",
+                        fontSize: "16px",
+                        textAlign: "center",
+                        borderRight: "1px solid #d3d3d3",
+                      }}
+                    >
+                      Course Name
+                    </th>
+                    <th
+                      style={{
+                        padding: "12px 20px",
+                        backgroundColor: "#C5E2F6",
+                        color: "#3498db",
+                        fontSize: "16px",
+                        textAlign: "center",
+                        borderRight: "1px solid #d3d3d3",
+                      }}
+                    >
+                      Course Version
+                    </th>
+                    <th
+                      style={{
+                        padding: "12px 20px",
+                        backgroundColor: "#C5E2F6",
+                        color: "#3498db",
+                        fontSize: "16px",
+                        textAlign: "center",
+                        borderRight: "1px solid #d3d3d3",
+                      }}
+                    >
+                      Instructor
+                    </th>
+                    <th
+                      style={{
+                        padding: "12px 20px",
+                        backgroundColor: "#C5E2F6",
+                        color: "#3498db",
+                        fontSize: "16px",
+                        textAlign: "center",
+                        borderRight: "1px solid #d3d3d3",
+                      }}
+                    >
+                      Year
+                    </th>
+                    <th
+                      style={{
+                        padding: "12px 20px",
+                        backgroundColor: "#C5E2F6",
+                        color: "#3498db",
+                        fontSize: "16px",
+                        textAlign: "center",
+                      }}
+                    >
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loading ? (
+                    <tr>
+                      <td colSpan="5" style={{ textAlign: "center" }}>
+                        Loading...
+                      </td>
+                    </tr>
+                  ) : rows.length > 0 ? (
+                    rows
+                  ) : (
+                    <tr>
+                      <td colSpan="5" style={{ textAlign: "center" }}>
+                        No instructors found
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </Table>
+            </div>
+          </Grid.Col>
+
+          <Grid.Col span={3}>
+            <Paper shadow="xs" p="md">
+              {/* ADD CURRICULUM button as a link */}
+              <Link
+                to="/programme_curriculum/acad_admin_add_course_instructor"
+                style={{ textDecoration: "none" }}
+              >
+                <Button
+                  variant="outline"
+                  fullWidth
+                  style={{ marginBottom: "20px" }}
+                >
+                  ADD INSTRUCTOR
+                </Button>
+              </Link>
+
+              {/* Filters */}
+              <TextInput
+                label="Course Code"
+                value={searchName}
+                onChange={(event) => setSearchName(event.currentTarget.value)}
+                mb="sm"
+              />
+              <TextInput
+                label="Instructor Name"
+                value={searchVersion}
+                onChange={(event) =>
+                  setSearchVersion(event.currentTarget.value)
+                }
+                mb="sm"
+              />
+              <TextInput
+                label="Year"
+                value={searchVersion}
+                onChange={(event) =>
+                  setSearchVersion(event.currentTarget.value)
+                }
+              />
+            </Paper>
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </MantineProvider>
+  );
+}
+
+export default Admin_view_all_course_instructors;
